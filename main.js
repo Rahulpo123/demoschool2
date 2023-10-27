7cf1c78e433b79fc00c734a8d1c6c0bd403e187a
Sike <117311129+Rahulpo123@users.noreply.github.com>
2023-10-27 19:48:09 +0530

    Add files via upload
    added file

---
+++ main.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:2000,
+    nav:false,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
